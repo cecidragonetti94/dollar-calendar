74aa701cf5f282c3d4b899d7f835cf99d02d4d5a
cecidragonetti94 <cecidragonetti@hotmail.com>
2021-10-21 11:55:18 -0300

    calendar

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -1,9 +1,20 @@
 .<template>
-  <v-container fluid grid-list-lg text-lg-left>
-    <v-row align-center justify-start >
+  <v-container fluid grid-list-lg text-lg-left mt-5>
+    <v-row align-center justify-start>
       <v-col>
+        <v-card>
+          <v-date-picker
+            v-model="date"
+            full-width
+            locale="es-ar"
+            :min="minDate"
+            :max="maxDate"
+          ></v-date-picker>
+        </v-card>
         <v-card color="error" dark>
-          <v-card-text class="display-1 text-center "> hola mundo </v-card-text>
+          <v-card-text class="display-1 text-center">
+            ${{value}} - {{ date }}</v-card-text
+          >
         </v-card>
       </v-col>
     </v-row>
@@ -11,7 +22,28 @@
 </template>
 
 <script>
-export default {};
+import  axios  from "axios";
+
+export default {
+  data() {
+    return {
+      date: "",
+      minDate: "1984",
+      maxDate: new Date().toISOString().substr(0, 10),
+      value: null
+    };
+  },
+  methods:{
+      async getDollar(day){
+         const data = await axios.get(`https://mindicador.cl/api/dolar/${day}`)
+         console.log(data.data.serie[0].valor)
+         this.value = await data.data.serie[0].valor
+      }
+  },
+  created(){
+     this.getDollar('20-10-2021')
+  }
+};
 </script>
 
 <style>
